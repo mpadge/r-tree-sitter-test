496113d45053a82598e429474c6b2b1487558dbe
mpadge <mark.padgham@email.com>
2024-10-02 17:08:16 +0200

    initial commit

---
+++ R/tree-sitter.R
@@ -0,0 +1,26 @@
+QUERY_FUNCTIONS <- r"(
+  (binary_operator
+      lhs: (identifier) @name
+      operator: "<-"
+      rhs: (function_definition) @fn
+  )
+)"
+QUERY_FUNCTIONS <- treesitter::query (treesitter.r::language (), QUERY_FUNCTIONS)
+
+#' @export
+get_captures <- function (path) {
+    path_r <- fs::path (path, "R")
+    paths <- fs::dir_ls (path_r, regexp = "\\.(r|R)$")
+    paths <- as.character (paths)
+
+    parser <- treesitter::parser (treesitter.r::language ())
+
+    lapply (paths, function (path) {
+        text <- brio::read_file (path)
+
+        tree <- treesitter::parser_parse (parser, text)
+        node <- treesitter::tree_root_node (tree)
+
+        treesitter::query_captures (QUERY_FUNCTIONS, node)
+    })
+}
